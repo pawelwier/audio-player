2384fb879d0640a0e9a4eb40771d2a38c8be2125
pawel <pawiercinski@gmail.com>
2025-02-16 12:56:04 +0100

    audio file state

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -4,11 +4,14 @@ use std::io::BufReader;
 use std::sync::Arc;
 use std::sync::Mutex;
 
+use eframe::egui::Response;
+use eframe::egui::Sense;
 use eframe::egui::{CentralPanel, Context};
 use eframe::{App, CreationContext, Frame};
 
 use rodio::{Decoder, Sink};
 
+use crate::audio::AudioState;
 use crate::file::file_system::read_file; 
 use crate::ui::render_elements::{render_file_options, render_stream_buttons};
 
@@ -18,7 +21,10 @@ struct AudioStream {
 
 pub struct AudioPlayer {
     stream: Arc<Mutex<AudioStream>>,
-    pub audio_path: String
+    // TODO: join?
+    pub base_path: String,
+    pub audio_path: String,
+    pub audio_state: AudioState
 }
 
 impl App for AudioPlayer {
@@ -34,7 +40,8 @@ impl App for AudioPlayer {
             // TODO: refactor and move logic out
             let (
                 play_button, pause_button, stop_button
-            ) = render_stream_buttons(ui);
+            ) = render_stream_buttons(ui, &self.audio_state);
+            // TODO: make btns active only when not selected
             if play_button.clicked() {
                 self.play_data(self.audio_path.to_owned());
             }
@@ -44,7 +51,7 @@ impl App for AudioPlayer {
             if stop_button.clicked() {
                 self.stop_data();
             }
-
+            
             render_file_options(ui, self);
         });
     }
@@ -54,12 +61,28 @@ impl AudioPlayer {
     pub fn new(_cc: &CreationContext<'_>, sink: Sink) -> Self {
         AudioPlayer { 
             stream: Arc::new(Mutex::new(AudioStream { sink })),
-            audio_path: "".to_owned()
+            audio_path: "".to_owned(),
+            // TODO: get path
+            base_path: "public".to_owned(),
+            audio_state: AudioState::NotSelected
         }
     }
 
     fn get_local_stream(&mut self) -> Arc<Mutex<AudioStream>> {
         self.stream.clone()
+    }
+
+    pub fn set_audio_state(&mut self, state: AudioState) -> () {
+        self.audio_state = state;
+    }
+
+    pub fn set_audio_state_on_option_change(&mut self, option: Response) -> () {
+        let option_response = option.interact(Sense::click());
+        if option_response.clicked() {
+            // TODO: make 'stop' only initially; 
+            // when audio playing, do not stop on select, stop and play on select -> play
+            self.stop_data();
+        }
     }
 
     pub fn play_data(&mut self, path: String) -> () {
@@ -72,15 +95,18 @@ impl AudioPlayer {
             thread::spawn(move || {
                 let _ = &local_stream.lock().unwrap().sink.append(source);
             });
+
+            self.set_audio_state(AudioState::Play);
         }
     }
 
     pub fn pause_data(&mut self) -> () {
-        // TODO: fix
-        // self.get_local_stream().lock().unwrap().sink.pause();
+        self.get_local_stream().lock().unwrap().sink.pause();
+        self.set_audio_state(AudioState::Pause);
     }
-
+    
     pub fn stop_data(&mut self) -> () {
         self.get_local_stream().lock().unwrap().sink.stop();
+        self.set_audio_state(AudioState::Stop);
     }
 }
--- src/ui/render_elements.rs
+++ src/ui/render_elements.rs
@@ -1,39 +1,52 @@
+use std::fs::DirEntry;
+
 use eframe::egui::{Button, CursorIcon, Response, Ui};
 
 use crate::app::AudioPlayer;
+use crate::audio::AudioState;
 use crate::file::file_system::get_files_from_dir;
 
 use super::layouts::vertical_align; 
 use super::text_utils::render_big_text;
 
-fn render_button_reg(ui: &mut Ui, text: &str) -> Response {
+fn render_button_reg(ui: &mut Ui, text: &str, disabled_states: Vec<AudioState>, state: &AudioState) -> Response {
+    let selected: bool = disabled_states.contains(&state);
     let btn_text = render_big_text(text);
     ui
         .add_sized(
             [100., 30.],
-            Button::new(btn_text)
+            Button::new(btn_text).selected(selected)
         )
         .on_hover_cursor(CursorIcon::PointingHand)
 }
 
-pub fn render_stream_buttons(ui: &mut Ui) -> (Response, Response, Response) {
+pub fn render_stream_buttons(ui: &mut Ui, state: &AudioState) -> (Response, Response, Response) {
+    println!("{:?}", &state);
     (
-        render_button_reg(ui, "Play!"),
-        render_button_reg(ui, "Pause"),
-        render_button_reg(ui, "Stop"),
+        // TODO: refactor
+        render_button_reg(ui, "Play!", vec![AudioState::NotSelected, AudioState::Play], &state),
+        render_button_reg(ui, "Pause", vec![AudioState::NotSelected, AudioState::Pause], &state),
+        render_button_reg(ui, "Stop", vec![AudioState::NotSelected, AudioState::Stop], &state),
     )
 }
 
-// TODO: get path
+fn get_file_path(file_name: &str, base_path: &str) -> String {
+    [base_path, &file_name].join("/")
+}
+
+fn render_file_option(ui: &mut Ui, app: &mut AudioPlayer, file: DirEntry) -> Response {
+    let file_name = file.file_name().to_str().unwrap().to_owned();
+    let file_path = get_file_path(&file_name, &app.base_path);
+    ui.selectable_value(&mut app.audio_path, file_path, file_name)
+}
+
 // TODO: filter .mp3 files
 pub fn render_file_options(ui: &mut Ui, app: &mut AudioPlayer) -> () {
     ui.with_layout(vertical_align(), |ui| {
-        let base_path = "public";
-        if let Ok(files) = get_files_from_dir(base_path) {
+        if let Ok(files) = get_files_from_dir(&app.base_path) {
             for file in files {
-                let file_name = file.file_name().to_str().unwrap().to_owned();
-                let file_path = [base_path, &file_name.clone()].join("/");
-                ui.selectable_value(&mut app.audio_path, file_path, file_name);
+                let option = render_file_option(ui, app, file);
+                app.set_audio_state_on_option_change(option);
             }
         }
     });
